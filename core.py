5179f1bd358097eae4cb78b5e3d54250ca43388f
dgalati427 <dgalati427@gmail.com>
2026-08-05 21:51:57 +1000

    Revert "Add daily-use improvements to the screener UI"
    This reverts commit 84ed1c2415d80251004916e847f0c1283cb6a22d.

--- core.py
+++ core.py
@@ -19,9 +19,8 @@ SP500_URL = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 ASX200_URL = "https://en.wikipedia.org/wiki/S%26P/ASX_200"
 
 EXCHANGE_LABELS = {
-    "NMS": "NASDAQ", "NGM": "NASDAQ", "NCM": "NASDAQ", "NAS": "NASDAQ",
-    "NYQ": "NYSE", "ASE": "NYSE American", "PCX": "NYSE Arca",
-    "BTS": "Cboe BZX", "BATS": "Cboe BZX",
+    "NMS": "NASDAQ", "NGM": "NASDAQ", "NCM": "NASDAQ",
+    "NYQ": "NYSE", "ASE": "NYSE American",
     "ASX": "ASX",
 }
 
